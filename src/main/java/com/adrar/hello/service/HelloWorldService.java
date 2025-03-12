f169a66b6956e1a6eb752d5b236048e5ba39311b
SophieHervieu <sophie-hervieu@live.fr>
2025-03-12 11:31:31 +0100

    exercice 1 springboot

---
+++ src/main/java/com/adrar/hello/service/HelloWorldService.java
@@ -0,0 +1,11 @@
+package com.adrar.hello.service;
+
+import com.adrar.hello.model.HelloWorld;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorldService {
+    public HelloWorld getHelloWorld() {
+        return new HelloWorld();
+    }
+}
